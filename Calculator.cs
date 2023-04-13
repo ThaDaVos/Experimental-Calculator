f5180bf7fd97b3e5bbbd9aa1c2f560619cf9fbfe
Dylan Vos <dylan@vos.email>
2023-04-13 10:23:28 +0200

    Add project files.

---
+++ Calculator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Experimental_Calculator
+{
+    internal class Calculator
+    {
+        private double _data;
+
+        public Calculator setData(double data)
+        {
+            this._data = data;
+
+            return this;
+        }
+
+        public double getData()
+        {
+            return _data;
+        }
+
+        public Calculator Add(double value)
+        {
+            this._data += value;
+
+            return this;
+        }
+
+        public Calculator Subtract(double value)
+        {
+            this._data -= value;
+
+            return this;
+        }
+
+        public Calculator Multiply(double value)
+        {
+            this._data *= value;
+
+            return this;
+        }
+
+        public Calculator Divide(double value)
+        {
+            this._data /= value;
+
+            return this;
+        }
+
+        public Calculator Power(int value)
+        {
+            this._data = Math.Pow(this._data, value);
+
+            return this;
+        }
+    }
+}
---
+++ Export.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace Experimental_Calculator
+{
+    public static class Export
+    {
+        [UnmanagedCallersOnly(EntryPoint = "Init", CallConvs = new[] { typeof(CallConvCdecl) })]
+        public static IntPtr Init(double data)
+        {
+            return GCHandle.ToIntPtr(
+                GCHandle.Alloc(
+                    new Calculator().setData(data)
+                    )
+                );
+        }
+
+        [UnmanagedCallersOnly(EntryPoint = "Result", CallConvs = new[] { typeof(CallConvCdecl) })]
+        [return: MarshalAs(UnmanagedType.R8)]
+        public static double Result(IntPtr calculator)
+        {
+            return As(calculator)?.getData() ?? 0.0;
+        }
+
+        [UnmanagedCallersOnly(EntryPoint = "Add", CallConvs = new[] { typeof(CallConvCdecl) })]
+        public static int Add(IntPtr calculator, double value)
+        {
+            As(calculator)?.Add(value);
+
+            return 0;
+        }
+
+        [UnmanagedCallersOnly(EntryPoint = "Subtract", CallConvs = new[] { typeof(CallConvCdecl) })]
+        public static int Subtract(IntPtr calculator, double value)
+        {
+            As(calculator)?.Subtract(value);
+
+            return 0;
+        }
+
+        [UnmanagedCallersOnly(EntryPoint = "Multiply", CallConvs = new[] { typeof(CallConvCdecl) })]
+        public static int Multiply(IntPtr calculator, double value)
+        {
+            As(calculator)?.Multiply(value);
+
+            return 0;
+        }
+
+        [UnmanagedCallersOnly(EntryPoint = "Divide", CallConvs = new[] { typeof(CallConvCdecl) })]
+        public static int Divide(IntPtr calculator, double value)
+        {
+            As(calculator)?.Divide(value);
+
+            return 0;
+        }
+
+        [UnmanagedCallersOnly(EntryPoint = "Power", CallConvs = new[] { typeof(CallConvCdecl) })]
+        public static int Power(IntPtr calculator, int value)
+        {
+            As(calculator)?.Power(value);
+
+            return 0;
+        }
+
+        private static Calculator? As(IntPtr ptr)
+        {
+            return (Calculator?)GCHandle.FromIntPtr(ptr).Target;
+        }
+    }
+}
